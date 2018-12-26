b440bef55f0cc016a872fe805ee2c10c0692234d
tao.gao <tao.gao@verycloud.cn>
2018-12-26 22:05:31 +0800

    add testThreadPool

---
+++ testThreadPool/src/threadpool.c
@@ -0,0 +1,137 @@
+#include "threadpool.h"
+
+void threadpool_init(threadpool_t *td, int threads)
+{
+	condition_init(&td->ready);
+	td->first = NULL;
+	td->last  = NULL;
+	td->counter = 0;
+	td->idle = 0;
+	td->max_threads = threads;
+	td->quit = 0;
+}
+
+void threadpool_destroy(threadpool_t *td)
+{
+	if (td->quit)
+		return;
+	condition_lock(&td->ready);
+	td->quit = 1;
+	if (td->counter > 0)
+	{
+		if (td->idle > 0)
+		{
+			condition_broadcast(&td->ready);
+		}
+		while(td->counter)
+		{
+			condition_wait(&td->ready);
+		}
+	}
+	condition_unlock(&td->ready);
+	condition_destroy(&td->ready);
+}
+
+void *thread_routine(void *arg)
+{
+	struct timespec abstime;
+	int timeout;
+	printf("thread %d is starting\n", (int)pthread_self());
+	threadpool_t *td = (threadpool_t *)arg;
+	while(1)
+	{
+		timeout = 0;
+		condition_lock(&td->ready);
+		td->idle++;
+		while(td->first == NULL && !td->quit)
+		{
+			printf("thread %d is waiting\n", (int)pthread_self());
+			clock_gettime(CLOCK_REALTIME, &abstime);
+			abstime.tv_sec += 2;
+			int status;
+			status = condition_timedwait(&td->ready, &abstime);
+			if (status == ETIMEDOUT)
+			{
+				timeout = 1;
+				break;
+			}
+		}
+
+		td->idle--;
+		if (td->first != NULL)
+		{
+			task_t *t = td->first;
+			td->first = td->first->next;
+			condition_unlock(&td->ready);
+			t->run(t->arg);
+			free(t);
+			condition_lock(&td->ready);
+		}
+
+		if (td->quit && td->first == NULL)
+		{
+			td->counter--;
+			if (td->counter == 0)
+				condition_signal(&td->ready);
+			condition_unlock(&td->ready);
+			break;
+		}
+
+		if (timeout == 1)
+		{
+			td->counter--;
+			condition_unlock(&td->ready);
+			break;
+		}
+		condition_unlock(&td->ready);
+	}
+
+	printf("thread %d is exiting\n", (int)pthread_self());
+	return NULL;
+}
+
+void threadpool_add_task(threadpool_t *td, void *(*task)(void *arg), void *arg)
+{
+	task_t *newtask	= (task_t *)malloc(sizeof(task_t));
+	newtask->run = task;
+	newtask->arg = arg;
+	newtask->next = NULL;
+
+	condition_lock(&td->ready);
+	if (td->first == NULL)
+	{
+		td->first = newtask;
+	}else
+	{
+		td->last->next = newtask; 
+	}
+	td->last = newtask;
+	
+	if (td->idle > 0)
+		condition_signal(&td->ready);
+	else if (td->counter < td->max_threads)
+	{
+		pthread_t tid;
+		pthread_create(&tid, NULL, thread_routine, td);
+		td->counter++;
+	}
+	condition_unlock(&td->ready);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ testThreadPool/src/threadpool.h
@@ -0,0 +1,35 @@
+#ifndef _THREADPOOL_H_
+#define _THREADPOOL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+
+#include "condition.h"
+
+typedef struct task
+{
+	void * (*run)(void *arg);
+	void *arg;
+	struct task *next;
+
+}task_t;
+
+typedef struct threadpool
+{
+	condition_t ready;
+	task_t *first;
+	task_t *last;	
+	int counter;	//当前线程
+	int idle;		//最大空闲线程
+	int max_threads;//最大允许开创的线程数
+	int quit;
+}threadpool_t;
+
+void threadpool_init(threadpool_t *td, int threads);
+void threadpool_destroy(threadpool_t *td);
+void threadpool_add_task(threadpool_t *td, void *(*task)(void *arg), void *arg);
+void *thread_routine(void *arg);
+#endif
